343bb1014de70571fe8ebb86c29bfee7c565e475
MehrdadKhanzadeh <mehrdadkh134@gmail.com>
2019-06-09 02:12:34 +0430

    feat: add cors and helmet to app

--- Implementation/ham-masir-api/app.js
+++ Implementation/ham-masir-api/app.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
 
 const routes = require('./routes')
 const { logger } = require('./utils')
 
 const app = express()
 
+app.use(cors())
+app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
