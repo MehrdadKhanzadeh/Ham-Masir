6cd38103c2a5a70990b72846d5eafcfbf8f74444
MehrdadKhanzadeh <mehrdadkh134@gmail.com>
2019-06-09 02:39:28 +0430

    fix: change home template

--- Implementation/ham-masir-pwa/src/views/Home.vue
+++ Implementation/ham-masir-pwa/src/views/Home.vue
@@ -1,16 +1,7 @@
 <template>
-<v-bottom-nav
-  absolute
-  value="value"
-  active.sync="value"
-  shift
-  color="red"
->
-<v-btn flat color="red" dark>
-  <span>text</span>
-  <v-icon>icon</v-icon>
-</v-btn>
-</v-bottom-nav>
+<v-layout row wrap>
+  
+</v-layout>
 </template>
 
 <script>
