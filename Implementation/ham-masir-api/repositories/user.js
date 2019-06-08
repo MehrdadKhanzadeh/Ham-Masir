0aefc5f0dfc2bda36173efd47623e2f8d552f581
MehrdadKhanzadeh <mehrdadkh134@gmail.com>
2019-06-09 01:55:07 +0430

    feat: add user repository

---
+++ Implementation/ham-masir-api/repositories/user.js
@@ -0,0 +1,50 @@
+const { logger } = require('../utils')
+
+const { User } = require('../models')
+
+const createUser = async (user) => {
+  try {
+    await User.create(user)
+
+    return {
+      isSuccessful: true
+    }
+  } catch (err) {
+    logger.error(err)
+
+    return {
+      isSuccessful: false,
+      statusCode: '200'
+    }
+  }
+}
+
+const findUser = async (user) => {
+  try {
+    let foundUser = await User.findOne(user)
+
+    if (foundUser !== null) {
+      return {
+        isSuccessful: true,
+        data: foundUser
+      }
+    } else {
+      return {
+        isSuccessful: false,
+        statusCode: '211'
+      }
+    }
+  } catch (err) {
+    logger.error(err)
+
+    return {
+      isSuccessful: false,
+      statusCode: '210'
+    }
+  }
+}
+
+module.exports = {
+  createUser,
+  findUser
+}
