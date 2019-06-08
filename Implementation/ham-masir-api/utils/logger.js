2c3b6d671f946720015df73985bc2ae1ddfe0583
MehrdadKhanzadeh <mehrdadkh134@gmail.com>
2019-06-08 15:14:22 +0430

    feat: add logger

---
+++ Implementation/ham-masir-api/utils/logger.js
@@ -0,0 +1,23 @@
+const winston = require('winston')
+const chalk = require('chalk')
+
+const logger = winston.createLogger({
+  level: 'silly',
+  format: winston.format.combine(
+    winston.format.colorize({
+      all: true
+    }),
+    winston.format.timestamp({
+      format: 'YYYY/MM/DD-HH:mm:ss'
+    }),
+    winston.format.align(),
+    winston.format.printf(info => {
+      return chalk.cyan(`\n[feedment-api(${chalk.magenta.bold(info.timestamp)} => ${chalk.bold(info.level)})]\t${chalk.bold(info.message)}`)
+    })
+  ),
+  transports: [
+    new winston.transports.Console()
+  ]
+})
+
+module.exports = logger
