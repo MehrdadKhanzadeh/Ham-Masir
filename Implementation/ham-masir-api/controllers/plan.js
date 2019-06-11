ce73322aa2d6e56ad8346845c4a5a4f7e17d5711
MehrdadKhanzadeh <mehrdadkh134@gmail.com>
2019-06-11 12:23:46 +0430

    feat: add plan controller

---
+++ Implementation/ham-masir-api/controllers/plan.js
@@ -0,0 +1,29 @@
+const { logger } = require('../utils')
+
+const { planService } = require('../services')
+
+const addPlan = async (req, res, next) => {
+  try {
+    const data = req.body
+
+    const username = data.username.toLowerCase().trim()
+    const path = data.path.trim()
+    const date = data.date.trim()
+    const time = data.time.trim()
+    const isDriver = data.isDriver
+    const repeatPattern = data.repeatPattern.trim()
+
+    res.json(await planService.addPlan(username, path, date, time, isDriver, repeatPattern))
+  } catch (err) {
+    logger.error(err)
+
+    res.json({
+      isSuccessful: false,
+      statusCode: '000'
+    })
+  }
+}
+
+module.exports = {
+  addPlan
+}
