bb2fdba630e74ac0580277eb5c5bb2c81850b89d
MehrdadKhanzadeh <mehrdadkh134@gmail.com>
2019-06-11 12:49:52 +0430

    fix: add plan router to router index

--- Implementation/ham-masir-api/routes/index.js
+++ Implementation/ham-masir-api/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
 const userRouter = require('./user')
+const planRouter = require('./plan')
 
 const router = express.Router()
 
 router.use('/user', userRouter)
+router.use('/plan', planRouter)
 
 module.exports = router
