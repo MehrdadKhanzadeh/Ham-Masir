3042c7cd91e2a8bf5308440f98e673840e6d3530
MehrdadKhanzadeh <mehrdadkh134@gmail.com>
2019-06-11 09:50:11 +0430

    fix: make path only an array of strings

--- Implementation/ham-masir-api/models/plan.js
+++ Implementation/ham-masir-api/models/plan.js
@@ -8,22 +8,8 @@ const planSchema = new mongoose.Schema({
   },
   path: {
     type: [{
-      latitude: {
-        type: Number,
-        min: 0,
-        max: 90,
-        required: true
-      },
-      longitude: {
-        type: Number,
-        min: 0,
-        max: 90,
-        required: true
-      },
-      postalAddress: {
         type: String,
         required: true
-      }
     }],
     validate: [arr => arr.length >= 2, 'Path should have at list 2 members.']
   },
@@ -47,9 +33,11 @@ const planSchema = new mongoose.Schema({
     type: String,
     enum: [
       'never',
+      'daily',
       'weekly',
       'monthly'
-    ]
+    ],
+    default: 'never'
   }
 })
 
